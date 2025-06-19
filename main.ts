b931878128f17a67daff34f037ae4535b17a3cbf
Jmeow <j@jmeow.net>
2025-06-19 12:26:39 -0500

    Click first non-italic link on page repeatedly

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-const chain = []
+import { DOMParser } from "jsr:@b-fuze/deno-dom"
+
+const chain: string[] = []
+
 while (true) {
     let page = prompt("Page:")!
     if (!page) {
@@ -21,3 +24,24 @@ while (true) {
     }
 }
 console.log("Starting path from " + chain[0])
+
+async function doPage(url: string) {
+    try {
+        const res = await fetch(url)
+        if (res.status == 200) {
+            const html = await res.text()
+            const parser = new DOMParser()
+            const doc = parser.parseFromString(html, "text/html")
+            const nextPage = "https://en.wikipedia.org" + doc.querySelector('#mw-content-text p:not(.hatnote) a[href^="/wiki/"]')?.getAttribute("href")
+            console.log(`Going from ${url} -> ${nextPage}`)
+            chain.push(nextPage)
+            doPage(chain[chain.length - 1])
+        } else {
+            console.log("Error " + res.status + " " + res.statusText)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+doPage(chain[0])
